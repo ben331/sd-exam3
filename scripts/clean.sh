71a7f7b02b5227ff04e9adf592f2bbe916e7f13e
Benjamin <benjamin.silva@insight4.tech>
2023-05-29 23:02:27 -0500

    refactor(docker): Change user of docker images tag by ben331

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e 'Borrando contenedores--------------------------------'
-# Eliminar todos los contenedores que empiecen por icesiops/
-docker rm -f $(docker ps -a | grep "icesiops/" | awk '{print $1}')
+# Eliminar todos los contenedores que empiecen por ben331/
+docker rm -f $(docker ps -a | grep "ben331/" | awk '{print $1}')
 docker rm -f $(docker ps -a | grep "consul" | awk '{print $1}')
 docker rm -f $(docker ps -a | grep "kafka" | awk '{print $1}')
 docker rm -f $(docker ps -a | grep "mongodb" | awk '{print $1}')
@@ -9,9 +9,9 @@ docker rm -f $(docker ps -a | grep "redis" | awk '{print $1}')
 docker rm -f $(docker ps -a | grep "gateway" | awk '{print $1}')
 
 
-echo -e '\n\nBorrando Imagenes--------------------------------'
-# Eliminar todas las imágenes que empiecen por icesiops/
-docker rmi -f $(docker images | grep "icesiops/" | awk '{print $3}')
+# echo -e '\n\nBorrando Imagenes--------------------------------'
+# Eliminar todas las imágenes que empiecen por ben331/
+# docker rmi -f $(docker images | grep "ben331/" | awk '{print $3}')
 
 # Mostrar estado actualizado
 echo -e '\n\nDOCKER PS----------------------------------------'
